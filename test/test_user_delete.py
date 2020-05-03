d538c0337af319a0b9fc97cbefc278809bc28f93
Gianmarco Cafferata <giancafferata@hotmail.com>
2020-05-02 22:41:11 -0300

    Tests fixed

--- test/test_user_delete.py
+++ test/test_user_delete.py
@@ -1,9 +1,17 @@
 from create_application import create_application
+from src.model.user import User
+from src.model.secured_password import SecuredPassword
 import unittest
+import json
 
 class TestUserRegistration(unittest.TestCase):
     def setUp(self) -> None:
-        self.app = create_application()
+        self.app, self.controller = create_application(return_controller=True)
+        admin_user = User(email="admin@admin.com", fullname="Admin",
+                          phone_number="11 1111-1111", photo="",
+                          secured_password=SecuredPassword.from_raw_password("admin"),
+                          admin=True)
+        self.controller.database.save_user(admin_user)
         self.app.testing = True
 
     def test_user_delete_for_missing_fields_error(self):
@@ -45,7 +53,13 @@ class TestUserRegistration(unittest.TestCase):
             self.assertEqual(response.status_code, 200)
             response = c.delete('/user', query_string={"email": "caropistillo@gmail.com"})
             self.assertEqual(response.status_code, 200)
-            response = c.get('/user', query_string={"email": "caropistillo@gmail.com"})
+
+            response = c.post('/user/login', data='{"email":"admin@admin.com", "password":"admin"}',
+                              headers={"Content-Type": "application/json"})
+            token = json.loads(response.data)["login_token"]
+
+            response = c.get('/user', query_string={"email": "caropistillo@gmail.com"},
+                             headers={"Authorization": "Bearer %s" % token})
             self.assertEqual(response.status_code, 404)
 
     def test_user_delete_and_update_non_existing_user_error(self):
--- test/test_user_update.py
+++ test/test_user_update.py
@@ -1,11 +1,18 @@
 from create_application import create_application
+from src.model.user import User
+from src.model.secured_password import SecuredPassword
 import unittest
 import json
 from src.model.secured_password import SecuredPassword
 
 class TestUserRegistration(unittest.TestCase):
     def setUp(self) -> None:
-        self.app = create_application()
+        self.app, self.controller = create_application(return_controller=True)
+        admin_user = User(email="admin@admin.com", fullname="Admin",
+                          phone_number="11 1111-1111", photo="",
+                          secured_password=SecuredPassword.from_raw_password("admin"),
+                          admin=True)
+        self.controller.database.save_user(admin_user)
         self.app.testing = True
 
     def test_user_update_no_json(self):
@@ -46,7 +53,13 @@ class TestUserRegistration(unittest.TestCase):
                              data='{"fullname":"Carolina Pistillo", "phone_number":"11 3263-7625", "photo":"caro.jpg", '
                                 '"password":"carolina"}', headers={"Content-Type": "application/json"})
             self.assertEqual(response.status_code, 200)
-            response = c.get('/user', query_string={"email": "caropistillo@gmail.com"})
+
+            response = c.post('/user/login', data='{"email":"caropistillo@gmail.com", "password":"carolina"}',
+                              headers={"Content-Type": "application/json"})
+            token = json.loads(response.data)["login_token"]
+
+            response = c.get('/user', query_string={"email": "caropistillo@gmail.com"},
+                             headers={"Authorization": "Bearer %s" % token})
             self.assertEqual(response.status_code, 200)
             response_json = json.loads(response.data)
             self.assertEqual(response_json["email"], "caropistillo@gmail.com")
